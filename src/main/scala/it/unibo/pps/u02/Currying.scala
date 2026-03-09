e8d1f93533b96a03b48c074842e0b58060778934
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-03-09 11:32:23 +0100

    exercise 3 and 4

--- src/main/scala/it/unibo/pps/u02/Currying.scala
+++ src/main/scala/it/unibo/pps/u02/Currying.scala
@@ -26,33 +26,5 @@ object Currying extends App :
   println(curriedMultAsFunction) // u02.Currying$$$Lambda$7/...
   println(curriedMultAsFunction(10)) // u02.Currying$$$Lambda$12/...
 
-  def divide(x: Double, y: Double) = (x, y) match
-    case (_, 0) => "Error: division by 0"
-    case _ => x / y
-
-  def divideCurried(x: Double)(y: Double) = (x, y) match
-    case (_, 0) => "Error: division by 0"
-    case _ => x / y
-
-  println(divide(3,4)) // 0.75
-  println(divide(3,0)) // Error: division by 0
-  println(divideCurried(3)(4)) // 0.75
-  println(divideCurried(3)) //rs$line$44$$$Lambda$2257/0x0000025db26cc800@1eb4bd92
-  println(divideCurried) //rs$line$45$$$Lambda$2260/0x0000025db26ccfc8@137d7b1f
-
-  val half = (x: Double) => divideCurried(x)(2.0)
-
-  println(half(4)) //2
-  println(half) // rs$line$48$$$Lambda$2261/0x0000025db26cdf90@41b40d93
-
-  val curriedDivAsFunction = (x: Double) => (y: Double) => (x, y) match
-    case (_, 0) => "Error: division by 0"
-    case _ => x / y
-
-  println(curriedDivAsFunction(2)(3)) // 0.6666666666666666
-  println(curriedDivAsFunction(2)(0)) // Error: division by 0
-  println(curriedDivAsFunction(2)) // rs$line$51$$$Lambda$2263/0x0000025db26cf720@25d2921d
-  println(curriedDivAsFunction) // rs$line$51$$$Lambda$2262/0x0000025db26cef58@30cce2bb
-
 
 
---
+++ src/main/scala/it/unibo/pps/u02/Exercise3.scala
@@ -0,0 +1,32 @@
+package it.unibo.pps.u02
+
+object Exercise3 extends App:
+  val sign: Int => String = _ match
+    case n if n >= 0 => "positive"
+    case _ => "negative"
+
+  def sign2(x: Int): String = x match
+    case n if n >= 0 => "positive"
+    case _ => "negative"
+
+  val neg: (String => Boolean) => String => Boolean = f => a => f(a) match
+    case n if n => false
+    case _ => true
+
+  val empty: String => Boolean = _ == "" // predicate on strings
+  val notEmpty = neg(empty) // which type of notEmpty?
+  println(notEmpty("foo"))// true
+  println(notEmpty("")) // false
+  println(notEmpty("foo") && !notEmpty(""))
+
+  def neg2(f: String => Boolean)(a: String): Boolean = f(a) match
+    case n if n => false
+    case _ => true
+
+  val empty1: String => Boolean = _ == "" // predicate on strings
+  val notEmpty1 = neg2(empty) // which type of notEmpty?
+  println(notEmpty1("foo")) // true
+  println(notEmpty1("")) // false
+  println(notEmpty1("foo") && !notEmpty1(""))
+
+
